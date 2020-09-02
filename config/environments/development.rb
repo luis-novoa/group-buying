3791710893ba95aef534f1aff7cc53913ff019d7
Luis Novoa <luis.matte.novoa@gmail.com>
2020-09-02 13:04:03 -0400

    Solves initial rubocop complains

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join('path/to').exist? # 'tmp', 'caching-dev.txt'
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
