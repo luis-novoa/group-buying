6f9e80afedff7c82ec04d51e77577482bf28c069
Luis Novoa <luis.matte.novoa@gmail.com>
2020-09-04 17:44:13 -0400

    Adds validations for Partner phones

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -20,4 +20,20 @@ class Partner < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 2, maximum: 75 },
             format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :phone1,
+            presence: true,
+            length: { minimum: 14, maximum: 15 },
+            format: /\(\d\d\)\ \d{4,5}-\d{4}/
+  validates :phone1_type, presence: true, inclusion: { in: ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp'] }
+  validates :phone2,
+            presence: true,
+            length: { minimum: 14, maximum: 15 },
+            format: /\(\d\d\)\ \d{4,5}-\d{4}/,
+            unless: -> { phone2_type.blank? }
+  validates :phone2, absence: true, if: -> { phone2_type.blank? }
+  validates :phone2_type,
+            presence: true,
+            inclusion: { in: ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp'] },
+            unless: -> { phone2.blank? }
+  validates :phone2_type, absence: true, if: -> { phone2.blank? }
 end
--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -34,6 +34,41 @@ RSpec.describe Partner, type: :model do
   it { is_expected.to allow_value('email@test.com').for(:email) }
   it { is_expected.to_not allow_value('emailtest.com', 'email@testcom', 'emailtestcom').for(:email) }
 
+  it { is_expected.to validate_presence_of(:phone1) }
+  it { is_expected.to validate_length_of(:phone1).is_at_least(14).is_at_most(15) }
+  it { is_expected.to allow_values('(66) 8536-7485', '(66) 98536-7485').for(:phone1) }
+  it { is_expected.to_not allow_value('a' * 14).for(:phone1) }
+
+  it { is_expected.to validate_presence_of(:phone1_type) }
+  it {
+    is_expected.to validate_inclusion_of(:phone1_type).in_array(
+      ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp']
+    )
+  }
+
+  context '.phone2 defined' do
+    it { is_expected.to validate_length_of(:phone2).is_at_least(14).is_at_most(15) }
+    it { is_expected.to allow_values('(66) 8536-7485', '(66) 98536-7485').for(:phone2) }
+    it { is_expected.to_not allow_values('a' * 14).for(:phone2) }
+    it { is_expected.to_not allow_value(nil).for(:phone2_type) }
+  end
+  context '.phone2 undefined' do
+    before(:each) { subject.phone2 = nil }
+    it { is_expected.to validate_absence_of(:phone2_type) }
+  end
+  context '.phone2_type defined' do
+    it { is_expected.to_not allow_value(nil).for(:phone2) }
+    it {
+      is_expected.to validate_inclusion_of(:phone2_type).in_array(
+        ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp']
+      )
+    }
+  end
+  context 'phone2_type undefined' do
+    before(:each) { subject.phone2_type = nil }
+    it { is_expected.to allow_value(nil).for(:phone2) }
+  end
+
   # it { is_expected.to validate_presence_of(:address) }
   # it { is_expected.to validate_length_of(:address).is_at_least(2).is_at_most(75) }
   # it { is_expected.to allow_values('bla bla, 78', 'bla bla, s/n', 'bla bla, 98, ap 200').for(:address) }
@@ -49,39 +84,4 @@ RSpec.describe Partner, type: :model do
   #        RN RO RS RR SC SE SP TO]
   #   )
   # }
-
-  # it { is_expected.to validate_presence_of(:phone1) }
-  # it { is_expected.to validate_length_of(:phone1).is_at_least(14).is_at_most(15) }
-  # it { is_expected.to allow_values('(66) 8536-7485', '(66) 98536-7485').for(:phone1) }
-  # it { is_expected.to_not allow_value('a' * 14).for(:phone1) }
-
-  # it { is_expected.to validate_presence_of(:phone1_type) }
-  # it {
-  #   is_expected.to validate_inclusion_of(:phone1_type).in_array(
-  #     ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp']
-  #   )
-  # }
-
-  # context '.phone2 defined' do
-  #   it { is_expected.to validate_length_of(:phone2).is_at_least(14).is_at_most(15) }
-  #   it { is_expected.to allow_values('(66) 8536-7485', '(66) 98536-7485').for(:phone2) }
-  #   it { is_expected.to_not allow_values('a' * 14).for(:phone2) }
-  #   it { is_expected.to_not allow_value(nil).for(:phone2_type) }
-  # end
-  # context '.phone2 undefined' do
-  #   before(:each) { subject.phone2 = nil }
-  #   it { is_expected.to validate_absence_of(:phone2_type) }
-  # end
-  # context '.phone2_type defined' do
-  #   it { is_expected.to_not allow_value(nil).for(:phone2) }
-  #   it {
-  #     is_expected.to validate_inclusion_of(:phone2_type).in_array(
-  #       ['Fixo', 'Celular com Whatsapp', 'Celular sem Whatsapp']
-  #     )
-  #   }
-  # end
-  # context 'phone2_type undefined' do
-  #   before(:each) { subject.phone2_type = nil }
-  #   it { is_expected.to allow_value(nil).for(:phone2) }
-  # end
 end
