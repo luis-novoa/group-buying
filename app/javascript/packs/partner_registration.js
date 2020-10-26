4b26479375a668d4cca2b68077bdd69a9ab2a04f
Luis Novoa <luis.matte.novoa@gmail.com>
2020-10-26 13:59:49 -0400

    Styles partners form

---
+++ app/javascript/packs/partner_registration.js
@@ -0,0 +1,15 @@
+const { addressFormat } = require('./utils');
+
+let hiddenFields = document.getElementsByClassName('hidden');
+let address = document.getElementById('partner_address');
+let addressStreet = document.getElementById('address_street');
+let addressNumber = document.getElementById('address_number');
+let addressInfo = document.getElementById('address_additional_info');
+
+address.style.display = 'none';
+console.log(hiddenFields);
+Array.prototype.forEach.call(hiddenFields, (field) => {
+  field.style.display = 'block';
+});
+
+addressFormat(address, addressStreet, addressNumber, addressInfo);
