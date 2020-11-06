6afa581ca4f3613d25b0a2ab09fba10cb7b10753
Luis Novoa <luis.matte.novoa@gmail.com>
2020-11-06 13:10:39 -0400

    Creates pagseguro notification handler

--- app/controllers/pagseguro_notifications_controller.rb
+++ app/controllers/pagseguro_notifications_controller.rb
@@ -1,3 +1,19 @@
 class PagseguroNotificationsController < ApplicationController
-  def create; end
+  def create
+    return unless params[:notificationType] == 'transaction'
+
+    credentials = URI.encode_www_form(
+      {
+        email: Rails.application.credentials.pagseguro[:email],
+        token: Rails.application.credentials.pagseguro[:sandbox_token]
+      }
+    )
+    transaction = HTTParty.get(
+      "https://ws.pagseguro.uol.com.br/v3/transactions/notifications/#{params[:notificationCode]}?#{credentials}"
+    )
+    return unless XMLUtils.get_attr(transaction.body, 'status') == '3'
+
+    payment = Payment.find_by(ref: XMLUtils.get_attr(transaction.body, 'reference'))
+    payment.orders.each { |order| order.update(status: 'Pago') }
+  end
 end
--- app/controllers/pagseguros_controller.rb
+++ app/controllers/pagseguros_controller.rb
@@ -7,10 +7,8 @@ class PagsegurosController < ApplicationController
       headers: { 'Content-Type' => 'application/x-www-form-urlencoded; charset=ISO-8859-1' },
       body: request_body
     )
-    p response
     payment_code = XMLUtils.get_attr(response.body, 'code')
-    p payment_code
     redirect_to "https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code=#{payment_code}"
-    # new_payment.update(ref: "PGTO#{new_payment.id}", code: payment_code)
+    new_payment.update(ref: "PGTO#{new_payment.id}", code: payment_code)
   end
 end
