868002f43fa204cbbe63fb2d28bcde9ff114019a
Luis Novoa <luis.matte.novoa@gmail.com>
2020-11-19 19:42:41 -0400

    Adds localization to pt-BR

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module GroupBuying
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.default_locale = :"pt-BR"
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
