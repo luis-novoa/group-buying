72d5ab39a8b2f95dd4c2465ea9b9b7dc46470ab2
Luis Novoa <luis.matte.novoa@gmail.com>
2020-12-18 13:49:38 -0400

    Adds new case to payment.status update

--- app/controllers/pagseguro_notifications_controller.rb
+++ app/controllers/pagseguro_notifications_controller.rb
@@ -15,7 +15,7 @@ class PagseguroNotificationsController < ActionController::API
     payment = Payment.find_by(ref: XMLUtils.get_attr(transaction.body, 'reference'))
     payment.update(status: transaction_status, code: XMLUtils.get_attr(transaction.body, 'code'))
     payment.orders.each { |order| order.update(status: 'Aguardando') } if transaction_status == '1'
-    payment.orders.each { |order| order.update(status: 'Pago') } if transaction_status == '3'
+    payment.orders.each { |order| order.update(status: 'Pago') } if %w[3 4].include?(transaction_status)
     payment.orders.each { |order| order.update(status: 'Cancelado') } if transaction_status == '7'
   end
 end
